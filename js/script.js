a9e5ab21e036f63cc7da864fd1fd4147ee6ce01a
Yan OrlovShatskykh <yan.orlov.shatskykh@gmail.com>
2020-06-18 01:40:26 +0300

    Created js file and all functionality for site, created mail.php and configured sending to mail

---
+++ js/script.js
@@ -0,0 +1,109 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+
+  const btnRent = document.getElementById('js-btn-rent');
+  const modal = document.getElementById('modal');
+  const btnCloseModal = document.getElementById('close');
+  const whatOrder = document.getElementById('js-whatOrder');
+  const playWithUs = document.getElementById('js-btn-playWithUs');
+  const playWithYou = document.getElementById('js-btn-playWithYou');
+  const catering = document.getElementById('js-btn-catering');
+  const sendBtn = document.querySelector('.brif__button');
+  const form = document.getElementById('brif-form');
+  
+  
+
+  
+  
+  
+
+
+  const openModal = event => {
+    // event.preventDefault();
+    const target = event.target;
+
+    modal.classList.add('modal_active');
+
+    modal.addEventListener('click', closeModal);
+    document.addEventListener('keydown', closeModal);
+
+    let order = target.dataset.order;
+
+    
+    whatOrder.dataset.whatOrder = order;
+    
+    // console.log(whatOrder.dataset.whatOrder);
+    
+  };
+
+
+  const closeModal = event => {
+    // event.preventDefault();
+    const target = event.target;
+
+    if(target.classList.contains('modal_active') ||
+      target.classList.contains('modal-dialog__close') ||
+      event.keyCode == 27) {
+
+        modal.classList.remove('modal_active');
+    }
+
+
+  };
+
+  // document.addEventListener('click', event => {
+  //   console.log(event.target);
+    
+  // });
+  const formData = new FormData();
+
+  const sendMail = (event) => {
+    event.preventDefault();
+
+    let sendedOrder = whatOrder.dataset.whatOrder;
+    let username = form[0].value;
+    let email = form[1].value;
+    let phone = form[2].value;
+    let comment = form[3].value;
+
+
+
+    // console.log(sendedOrder, username, email, phone, comment);
+          
+    formData.append('sendedOrder', sendedOrder);
+    formData.append('username', username);
+    formData.append('email', email);
+    formData.append('phone', phone);
+    formData.append('comment', comment);
+
+
+    // console.dir(formData);
+    
+    fetch("php/mail.php", {
+      method: "POST",
+      body: formData
+    })
+    .then(response => response.text())
+    .then(response => console.log(response));
+
+
+
+
+
+
+    
+  };
+
+  // console.dir(form);
+  
+
+  btnRent.addEventListener('click', openModal);
+  playWithUs.addEventListener('click', openModal);
+  playWithYou.addEventListener('click', openModal);
+  catering.addEventListener('click', openModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  // sendBtn.addEventListener('click', sendMail);
+  form.addEventListener('submit', sendMail);
+  
+});
--- php/mail.php
+++ php/mail.php
@@ -1,64 +1,35 @@
 <?php
-    session_start();
-    require_once '../db/db.php';
 
-    if(isset($_POST['order'])) {
-        $username = $_POST['username'];
-        $tel = $_POST['tel'];
-        $email = $_POST['email'];
-        $connect->query("INSERT INTO `order` (`username`, `email`, `phone`) VALUES ('$username', '$email', '$tel')");
-        $lastId = $connect->query("SELECT MAX(id) FROM `order`")->fetch(PDO::FETCH_ASSOC);
-        $lastId = $lastId['MAX(id)'];
+        if(isset($_POST['sendedOrder'])) {
 
-        foreach ($_SESSION['cart'] as $key => $product) {
-            $prod = $connect->query("SELECT * FROM products WHERE id='$key'")->fetch(PDO::FETCH_ASSOC);
-            $price = $product['price'] * $product['quantity'];
-            $connect->query("INSERT INTO `goods` (`title`, `rus_name`, `price`, `total_price`, `quantity`, `id_of_good`, `buyer`) VALUES ('{$prod['title']}', '{$prod['rus_name']}', '{$prod['price']}', '$price', '{$_SESSION['cart'][$product['id']]['quantity']}', '{$product['id']}', '$username')");
-            $lastIdGoods = $connect->query("SELECT MAX(id) FROM `goods`")->fetch(PDO::FETCH_ASSOC);
-            $lastIdGoods = $lastIdGoods['MAX(id)'];
-            $connect->query("UPDATE `goods` SET `id_of_order` = '$lastId' WHERE id='$lastIdGoods'");
-        }
+                $sendedOrder = $_POST['sendedOrder'];
+                $username = $_POST['username'];
+                $email = $_POST['email'];
+                $phone = $_POST['phone'];
+                $comment = $_POST['comment'];
 
-        $message = "<h2>Здравствуйте $username, Ваш заказ под номером $lastId принят.</h2>";
-        $message .= "<h3>Состав заказа:</h3>";
 
-        foreach ($_SESSION['cart'] as $product) {
-            $message .= "<p>{$product['rus_name']}, в количестве {$product['quantity']} шт.</p>";
-        }
-
-        $message .= "<p>Сумма заказа: {$_SESSION['totalPrice']} гривен.</p>";
-        $subject = "Ваш заказ под номером $lastId принят.";
+                // $data = $_POST;
+                
+                
 
 //        create email message for admin
-        $admSubject = "Поступил новый заказ под номером $lastId.";
-        $admMessage = "<h2>Здравствуйте, поступил новый заказ под номером $lastId.</h2>";
-        $admMessage .= "<h3>Состав заказа:</h3>";
-
-        foreach ($_SESSION['cart'] as $product) {
-            $admMessage .= "<p>{$product['rus_name']}, в количестве {$product['quantity']} шт.</p>";
-        }
-
-        $admMessage .= "<p>Сумма заказа: {$_SESSION['totalPrice']} гривен.</p>";
-        $admMessage .= "<h3>Имя клиента - $username, номер телефона - $tel, почта - $email.</h3>";
-        $admMessage .= "<h2>Номер заказа - $lastId.</h2>";
-        $admEmail = "yan.orlov.shatskykh@gmail.com";
-        $headers = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-
-//        send messages for user and admin
-        mail($email, $subject, $message, $headers);
-        mail($admEmail, $admSubject, $admMessage, $headers);
-
-        unset($_SESSION['totalQuantity']);
-        unset($_SESSION['totalPrice']);
-        unset($_SESSION['cart']);
-        $_SESSION['order'] = $lastId;
-        header("Location: {$_SERVER['HTTP_REFERER']}");
-
-
-
+                $admSubject = "Надійшло нове замовлення!";
+                $admMessage = "<h2>Вітаю! Надійшло нове замовлення.</h2>";
+                $admMessage .= "<h3>$sendedOrder</h3>";
+                $admMessage .= "<h3>Ім'я замовника - $username, номер телефону - $phone, email - $email.</h3>";
+                $admEmail = "yan.orlov.shatskykh@gmail.com";
+                $headers = 'MIME-Version: 1.0' . "\r\n";
+                $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+                
+                //        send messages for user and admin
+                mail($admEmail, $admSubject, $admMessage, $headers);
+                
+                
+                // echo json_encode($admMessage, JSON_UNESCAPED_UNICODE);
+                
         
 // $data = $_POST['username'];
 // echo json_encode($data, JSON_UNESCAPED_UNICODE);
 // echo $data;
-    }
+        }
